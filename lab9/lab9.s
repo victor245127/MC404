bd6e3b8850547ea8ef9c06c5329872a005684267
vmm <v245127@dac.unicamp.br>
2025-09-30 20:13:40 -0300

    Funcionando para casos -1 apenas.

--- lab9/lab9.s
+++ lab9/lab9.s
@@ -6,7 +6,7 @@ buffer_out: .space 5
 .text
 .globl _start
 
-parsing:
+parsing_int:
     mv t6, a1
     li a0, 0
     li t4, 10 
@@ -33,12 +33,75 @@ parsing:
     blt x0, t0, 2b
     mul a0, a0, t3 # multiplica por +/-1 dependendo do sinal
 
-return:
+3:
+    ret
+
+
+
+parsing_ascii:
+    mv t0, a1 # buffer de saida em t0
+    li t1, 100
+    li t2, 10
+
+    blt s3, x0, 1f # se nao houver indice, sera -1
+    blt s3, t2, 2f # se indice for menor que 10, sera apenas 1 digito
+    blt s3, t1, 3f # se indice for menor que 100, sera apenas 2 digitos
+    li t4, 3 # se indice > 100, nao pode ser >= 1000
+    j 4f
+    
+1: 
+    li t1, 45 # '-'
+    li t3, 49 # '1'
+    sb t1, 0(t0) # '-1\n'
+    sb t3, 1(t0)
+    sb t2, 2(t0)
+    j 5f
+
+2:
+    li t3, 1 # divisor inicial sendo 1
+    li t4, 1
+    j 4f
+
+3:
+    mv t3, t2 # divisor inicial sendo 10
+    li t4, 2
+
+4:
+    div a0, s4, t3 # num / div inicial
+    rem a2, s4, t3 # resto da divisao
+    addi a2, a2, 48 # ascii
+    sb t2, 0(t0) # armazena digito no buffer
+    addi t0, t0, 1
+    addi t4, t4, -1 # i--
+    blt x0, t4, 4b # t4 > 0
+    sb t2, 0(t0) # '\n' no final
+
+5:
     ret
 
 
 percorrendo_lista:
-    
+    la a0, head_node # carrega cabeca da lista em a0
+    li s3, -1 # inicialmente output é -1 e continuara caso nao ache a soma
+    li t1, 0 # indice inicial da lista
+
+1:
+    lw a1, 0(a0) # primeiro numero da soma
+    lw a2, 4(a0) # segundo numero da soma
+    lw t2, 8(a0) # ponteiro para o proximo no
+    add a3, a1, a2 
+    beq s1, a3, 2f # se o input for igual a soma, retorna o indice
+    beq t2, x0, 3f # caso proximo nó seja 0 e nao tiver achado a soma, retorna -1
+
+    addi a0, a0, 12 # pula para o primeiro numero do proximo no
+    addi t1, t1, 1 # vai pra proximo indice
+    j 1b
+
+2:
+    mv s3, t1 # armazena indice da soma em s3
+
+3:
+    ret
 
 
 
@@ -50,8 +113,6 @@ _start:
     ecall
     mv s2, a0 # salva fd em s2
 
-
-
     li a0, 0
     la a1, buffer
     li a2, 7
@@ -61,8 +122,13 @@ _start:
 
     mv a0, s0
     la a1, buffer
-    call parsing
+    call parsing_int
     mv s1, a0 # soma em s1
+
+    call percorrendo_lista
+
+    la a1, buffer_out
+    call parsing_ascii
 
 
 write:
