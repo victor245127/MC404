2dbc4cea9de82381ced288a60be1957fa896d9d3
vmm <v245127@dac.unicamp.br>
2025-10-01 14:19:15 -0300

    Lendo buffer certo, passando pixels errado

--- lab8/lab8a.s
+++ lab8/lab8a.s
@@ -1,80 +1,40 @@
 .data
 input_file: .asciz "image.pgm"
-buffer: .space 262400 # tamanho maximo das imagens em bytes
+buffer: .space 262160
 
 .text
 .globl _start
 
-
-_start:
-    la a0, input_file 
-    li a1, 0
-    li a2, 0
-    li a7, 1024 # syscall de open pra abrir o arquivo
-    ecall
-    mv s0, a0 # fd do arquivo
-
-    mv a0, s0
-    la a1, buffer
-    li a2, 262400 # max de bytes a serem lidos
-    li a7, 63
-    ecall # syscall read
-    mv s1, a0 # numero de bytes lidos salvo
-
-close:
-    li a0, 3
-    li a7, 57
-    ecall # syscall close pra fechar o arquivo
-
-setCanvasSize:
-    la a1, buffer
-    call parse_int
-
-    mv s4, a1 # posicao inicial da leitura dos bits
-    mv a0, s2 # largura
-    mv a1, s3 # altura
-
-    li a7, 2201 # syscall para definir tamanho do canvas
-    ecall
-
-montar_imagem:
-    call pixel_by_pixel
-
-end:
-    li a0, 0
-    li a1, 93
-    ecall # syscall exit
-
-
-
 parse_int:
-    addi a1, a1, 3 # pula o 'P5\n'
-    li a0, 0
+    mv t6, a1 # move buffer para t6
+    addi t6, t6, 3 # pula P5 whitespace
     li t3, 10
-    li t5, 20 # ' '
+    li t4, 48
+    li t5, 58
+    li a0, 0
 
 1:
-    lbu t0, 0(a1) # caractere
-    addi t0, t0, -48 # numero
-    mul a0, a0, t3 # multiplica valor anterior por 10
-    add a0, a0, t0 # adiciona digito
-    addi a1, a1, 1 # anda no buffer
+    lb t0, 0(t6) # numero em t0
+    blt t0, t4, 2f # < '0', salva numero
+    bge t0, t5, 2f # numero > '9', salva numero
 
-    lbu t4, 0(a1) # carrega caractere seguinte
-    beq t4, t5, 2f # se for ' ' salva largura e comeca a calcular altura
-    beq t4, t3, 3f # se for '\n' fim
+    mul a0, a0, t3 # multiplica numero antigo por 10
+    addi t0, t0, -48
+    add a0, a0, t0 # adiciona digito
+    addi t6, t6, 1 # anda no buffer
     j 1b
 
 2:
-    mv s2, a0 # salva largura
-    addi a1, a1, 1
+    bne s2, x0, 3f # se s2 nao for 0, salva altura
+    mv s2, a0 # salva largura em s2
+    addi t6, t6, 1 # anda no buffer
     j 1b
 
 3:
-    mv s3, a0 # salva altura
-    addi a1, a1, 5 # pula '255\n'
+    mv s3, a0 # salva altura em s3
+    addi t6, t6, 5 # pula maxval + whitespace
+    mv s4, t6 # salva posicao inicial dos pixels em s4
     ret
-
 
 
 pixel_by_pixel:
@@ -88,7 +48,7 @@ pixel_by_pixel:
     mul t0, a1, s2 # offset de (x,y), y * largura 
     add t0, t0, a0  # t0 + x
     add t1, s4, t0 # endereço do pixel em t1 (posição inicial + offset)
-    lbu t2, 0(t1) # carrega byte de (x,y) 
+    lb t2, 0(t1) # carrega byte de (x,y) 
 
     slli t3, t2, 24 # R
     slli t4, t2, 16 # G
@@ -109,3 +69,40 @@ pixel_by_pixel:
 return:
     ret
 
+
+
+_start:
+    la a0, input_file
+    li a1, 0
+    li a2, 0
+    li a7, 1024 # syscall open
+    ecall
+    mv s0, a0
+
+    mv a0, s0
+    la a1, buffer
+    li a2, 262160
+    li a7, 63 # syscall read
+    ecall
+    mv s1, a0 # tamanho lido
+
+setCanvasSize:
+    la a1, buffer
+    call parse_int
+
+    mv a0, s2
+    mv a1, s3 
+    li a7, 2201 # syscall setCanvasSize
+    ecall 
+
+montar_imagem:
+    call pixel_by_pixel
+
+end:
+    li a0, 3
+    li a7, 57 # syscall close
+    ecall
+
+    li a7, 93 # syscall exit
+    li a0, 0
+    ecall
